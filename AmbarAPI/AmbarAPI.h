dea38e71a3a162d971a367693b374156676a0f11
teythedev <emrethedev@gmail.com>
2022-10-17 21:01:03 +0300

    Initial Commit

---
+++ AmbarAPI/AmbarAPI.h
@@ -0,0 +1,18 @@
+//
+//  AmbarAPI.h
+//  AmbarAPI
+//
+//  Created by Tugay on 17.10.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AmbarAPI.
+FOUNDATION_EXPORT double AmbarAPIVersionNumber;
+
+//! Project version string for AmbarAPI.
+FOUNDATION_EXPORT const unsigned char AmbarAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmbarAPI/PublicHeader.h>
+
+
